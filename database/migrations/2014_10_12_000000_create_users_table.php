147ecdb68c204a2c2a41f460b9a2890a39d342e6
GoldyWy <goldywidiyanto@gmail.com>
2020-06-05 17:59:33 +0700

    awal

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 50);
+            $table->string('email', 50)->unique();
+            $table->string('password', 191);
+            $table->string('latitude', 100);
+            $table->string('longitude', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
